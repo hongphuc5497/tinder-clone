0f76485a6e985a672981a68cbc00704926b2aa31
Hong Phuc <og.viet.003.dev@gmail.com>
2019-11-19 13:26:50 +0700

    Add bootstrap, jquery, popper.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provive',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
+    })
+  )
+
 module.exports = environment
