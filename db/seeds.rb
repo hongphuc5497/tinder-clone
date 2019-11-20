f69e9cf384d214437bd31b724787cdfd8dac53bc
Hong Phuc <og.viet.003.dev@gmail.com>
2019-11-20 11:44:55 +0700

    Add browse controller, config basic browse page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Account.create([
+  { first_name: "test", last_name: "2", username: "test_2", email: "test_2@gmail.com", password: "123456" },
+  { first_name: "test", last_name: "3", username: "test_3", email: "test_3@gmail.com", password: "123456" },
+  { first_name: "test", last_name: "4", username: "test_4", email: "test_4@gmail.com", password: "123456" },
+  { first_name: "test", last_name: "5", username: "test_5", email: "test_5@gmail.com", password: "123456" }
+])
