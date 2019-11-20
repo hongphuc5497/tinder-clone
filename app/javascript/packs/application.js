129cf6fac69b5e55efbb1cd93b733f8888359591
Hong Phuc <og.viet.003.dev@gmail.com>
2019-11-20 15:34:47 +0700

    Add fontawesome icon to browse page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start()
 
 require("bootstrap")
 require("../includes/slide.js")
+import "@fortawesome/fontawesome-free/css/all.css";
+// import "@fortawesome/fontawesome-free/js/all";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
