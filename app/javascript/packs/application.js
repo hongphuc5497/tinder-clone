cf9aab962546774a838056fe1fb68abfaaf32c28
Hong Phuc <og.viet.003.dev@gmail.com>
2019-11-22 11:05:24 +0700

    Create matches.js, implement function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 
 require("bootstrap")
 require("../includes/slide.js")
+require("../includes/matches.js")
 import "@fortawesome/fontawesome-free/css/all.css";
 // import "@fortawesome/fontawesome-free/js/all";
 // Uncomment to copy all static images under ../images to the output folder and reference
